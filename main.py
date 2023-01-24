6ffd5290754bfcfd7fc2e914b25e41aed85d00fb
Valery Muradyan <v_muradyan@list.ru>
2023-01-24 21:19:10 +0300

    Добавил докстринги

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция, подсчитывающая нанесенный урон."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон'
                 ' противнику равный {5 + randint(3, 5)}')
@@ -15,6 +16,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция, применяющая действие - защита."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -24,6 +26,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция применяющая действие - атака."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 ' «Выносливость {80 + 25}»')
@@ -34,6 +37,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция, позволяющая выбрать действие персонажа или выйти из игры."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -58,6 +62,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция, которая подтверждает выбор персонажа после ввода."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -81,6 +86,7 @@ def choice_char_class() -> str:
 
 
 if __name__ == '__main__':
+    """Функция, позволяющая игроку ввести имя героя."""
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
